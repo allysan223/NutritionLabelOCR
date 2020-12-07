c4d97984bccad1628a3087308e3a3d3d90075daa
allysan223 <allysab@uw.edu>
2020-12-06 19:36:03 -0800

    first commit

---
+++ nutrition_label.py
@@ -0,0 +1,25 @@
+import sys
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib 
+import matplotlib.pyplot as plt
+from cv2 import cv2
+import math
+import pytesseract
+import matplotlib.image as mpimg
+from os.path import dirname, join
+
+current_dir = dirname(__file__)
+file_path = join(current_dir, "./label.png")
+
+img = cv2.imread(file_path)
+#img = cv2.imread('label.png')
+# plt.imshow(img)
+# plt.show()
+
+# Adding custom options
+custom_config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(img, config=custom_config)
+print(text)
