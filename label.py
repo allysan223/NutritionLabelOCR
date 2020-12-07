8ce93cbfa58d156fd44b5e758cdfaa7dcdefdfc8
allysan223 <allysab@uw.edu>
2020-12-06 20:27:30 -0800

    parse label

---
+++ label.py
@@ -0,0 +1,37 @@
+from parse import *
+
+class Label():
+    """
+    Serving Size cup
+    Servings per container 4
+    Calories 250 FatCal 120
+    Total Fat 130 20%
+    Sat Fat 99 40%
+    Cholesterol 28mg 12%
+    ‘Sodium 55mg 2%
+    Total Carbohydrate 30g 12%
+    Dietary Fiber 2g
+    Sugars 239
+    Protein 4g 8%
+    Ingredients: Cream, Skim Mik, Liquid
+    ‘Sugar, Water, Egg Yolks, Brown Sugar,
+    Mikfat, Peanut Ol, Sugar, Butter, Salt,
+    Carrageenan, Vanilla Extract.
+    """
+    def __init__(self, text):
+        self.text = text
+        self.servingSize = search('Serving Size {:w}', text)
+        self.servingsPer = search('Servings per container {:d}', text)
+        self.calories = search('Calories {:d}', text)
+        self.fatCals = search('FatCal {:d}', text)
+        self.totalFat = search('Total Fat {:d} {:w}', text)
+        self.satFat = search('Sat Fat {:d} {:w}', text)
+        self.cholesterol = search('Cholesterol {:d} {:w}', text)
+        self.sodium = search('Sodium {:d} {:w}', text)
+        self.totalCarbs = search('Total Carbohydrate {:d} {:w}', text)
+        self.fiber = search('Fiber {:w}', text)
+        self.sugars = search('Sugars {:d}', text)
+        self.protien = search('Protein {:d} {:w}', text)
+        self.ingredients = text.split("Ingredients: ")[1]
+
+    
--- nutrition_label.py
+++ nutrition_label.py
@@ -10,6 +10,7 @@ import math
 import pytesseract
 import matplotlib.image as mpimg
 from os.path import dirname, join
+from label import *
 
 current_dir = dirname(__file__)
 file_path = join(current_dir, "./label.png")
@@ -23,3 +24,6 @@ img = cv2.imread(file_path)
 custom_config = r'--oem 3 --psm 6'
 text = pytesseract.image_to_string(img, config=custom_config)
 print(text)
+
+label1 = Label(text)
+print(label1.ingredients)
